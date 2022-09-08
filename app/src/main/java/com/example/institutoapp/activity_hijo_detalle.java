39982dd3fdfa793f849f8d64e290760d96e91c99
Ignacio Malfavon Gutierrez <ignacio_96@live.com.mx>
2022-09-08 01:52:58 -0500

    declaramos variables TextView de Detalle hijo en la clase de java

--- app/src/main/java/com/example/institutoapp/activity_hijo_detalle.java
+++ app/src/main/java/com/example/institutoapp/activity_hijo_detalle.java
@@ -3,12 +3,17 @@ package com.example.institutoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_hijo_detalle extends AppCompatActivity {
-
+private TextView txtEstadoActual,txtEstadoActualN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hijo_detalle);
+        txtEstadoActual = findViewById(R.id.txtDetalleHijoEstadoActual);
+        txtEstadoActualN = findViewById(R.id.txtDetalleHijoEstadoActualN);
+        txtEstadoActualN.setText("2");
+        txtEstadoActual.setText("Activo");
     }
 }
